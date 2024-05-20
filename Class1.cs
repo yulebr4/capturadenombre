da7c96fa04f5b373c3ecd1f9c7206aea712768e5
MOISES <MOISES@DESKTOP-17FT0L7>
2024-05-19 23:43:44 -0400

    Agregar archivos de proyecto.

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace capturadenombre
+{
+    class Program 
+    {
+
+        static void Main(string[] args) 
+        {
+
+            Console.WriteLine("Esto es un programa para captura nombres\n");
+            string nombre;
+            string apellido;
+            Console.WriteLine("Introduzca su nombre\n");
+            nombre=Console.ReadLine();
+            apellido = Console.ReadLine();
+            Console.WriteLine("El nombre completo ingresado es: " + nombre + " " + apellido);
+
+        }
+    }  
+}
